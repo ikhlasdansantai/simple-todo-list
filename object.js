770b93fe6a4bd46f4426324a4b9526df9dcfe117
ikhlasdansantai <initesting5@gamil.com>
2022-12-03 14:39:31 +0700

    file clear

--- object.js
+++
@@ -1,7 +0,0 @@
-function checkSign(num) {
-  return num === 0 ? "zero" : (num => 0) ? "positive" : (num <= 0) ? "negative" : "positive";
-}
-
-console.log(checkSign(0));
-console.log(checkSign(10));
-console.log(checkSign(-10));
